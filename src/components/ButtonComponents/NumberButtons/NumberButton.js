c97f6817a9278e85fa733d03387c77a6b832298a
devin-bielejec <devin.bieleje@gmail.com>
2019-08-12 14:07:20 -0400

    Working on everything, trying to understand the conceptual aspect

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,13 @@
 import React from "react";
+// import { prependOnceListener } from "cluster";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
   return (
-    <>
+    <button>
+      {props.number}
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    </button>
   );
 };
+
+export {NumberButton};
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
+import {NumberButton} from "./NumberButton";
 
 //Import your array data to from the provided data file
 import {numbers} from "../src/data.js";
@@ -8,9 +9,13 @@ import {numbers} from "../src/data.js";
 const Numbers = () => {
   // STEP 2 - add the imported data to state
   const [numberState, setNumberState] = useState(numbers);
+
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
+      {numbers.map( (number) => {
+      return <NumberButton number={number} />
+      }
+      /* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
     </div>
