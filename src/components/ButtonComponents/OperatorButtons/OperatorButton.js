df22943f40abd99b8c1900edb33c132daa05b05a
devin-bielejec <devin.bieleje@gmail.com>
2019-08-12 15:09:15 -0400

    Styled some buttons, got the display to display

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const OperatorButton = (props) => {
   return (
-    <button className="Operator" value={props.value}>
+    <button className="operator" value={props.value}>
       {props.char}
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </button>
